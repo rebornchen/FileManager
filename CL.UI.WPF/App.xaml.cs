7c556c3499937d6dc4c96fe339f85dbd9c5f6167
Charlie <chenl_work@126.com>
2016-10-17 14:34:21 +0800

    create project CL.UI.WPF, reference 'mvvm light' and 'MahApps.Metro' from NuGet; nuget command flow: Install-Package MahApps.Metro -Version 1.3.0

---
+++ CL.UI.WPF/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using GalaSoft.MvvmLight.Threading;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+
+
+namespace CL.UI.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static App()
+        {
+            DispatcherHelper.Initialize();
+        }
+
+        //[STAThread]
+        //public static void Main()
+        //{
+        //    App myApp = new App();
+        //    myApp.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+        //    myApp.Run();
+        //}
+    }
+}
---
+++ CL.UI.WPF/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using CL.UI.WPF.ViewModel;
+using MahApps.Metro.Controls;
+
+namespace CL.UI.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+
+        
+    }
+}
