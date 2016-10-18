51ced7dbe7571ea12aa01068beb2172051a71c52
Charlie <chenl_work@126.com>
2016-10-18 10:38:34 +0800

    update MainWindow for change themes function

--- CL.UI.WPF/ViewModel/MainViewModel.cs
+++ CL.UI.WPF/ViewModel/MainViewModel.cs
@@ -1,5 +1,12 @@
 ﻿using GalaSoft.MvvmLight;
 using CL.UI.WPF.Model;
+using System.Collections.Generic;
+using System.Windows.Media;
+using System.Windows.Input;
+using MahApps.Metro;
+using System.Windows;
+using System.Linq;
+using GalaSoft.MvvmLight.Command;
 
 namespace CL.UI.WPF.ViewModel
 {
@@ -11,6 +18,7 @@ namespace CL.UI.WPF.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
+        #region 示例部分
         private readonly IDataService _dataService;
 
         /// <summary>
@@ -54,6 +62,77 @@ namespace CL.UI.WPF.ViewModel
                     WelcomeTitle = item.Title;
                 });
         }
+        #endregion
+
+
+        #region 界面主题
+        /// <summary>
+        /// The <see cref="AppThemes" /> property's name.
+        /// </summary>
+        public const string AppThemesPropertyName = "AppThemes";
+
+        private List<AppThemeMenuData> _appThemes = ThemeManager.AppThemes
+                                           .Select(a => new AppThemeMenuData() { Name = a.Name, BorderColorBrush = a.Resources["BlackColorBrush"] as Brush, ColorBrush = a.Resources["WhiteColorBrush"] as Brush })
+                                           .ToList();
+
+        /// <summary>
+        /// Sets and gets the _app property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public List<AppThemeMenuData> AppThemes
+        {
+            get
+            {
+                return _appThemes;
+            }
+
+            set
+            {
+                if (_appThemes == value)
+                {
+                    return;
+                }
+
+                _appThemes = value;
+                RaisePropertyChanged(() => _appThemes);
+            }
+        }
+        #endregion
+
+
+        #region 界面颜色
+        /// <summary>
+        /// The <see cref="AccentColors" /> property's name.
+        /// </summary>
+        public const string AccentColorsPropertyName = "AccentColors";
+
+        private List<AccentColorMenuData> _accentColors = ThemeManager.Accents
+                                            .Select(a => new AccentColorMenuData() { Name = a.Name, ColorBrush = a.Resources["AccentColorBrush"] as Brush })
+                                            .ToList();
+
+        /// <summary>
+        /// Sets and gets the AccentColors property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public List<AccentColorMenuData> AccentColors
+        {
+            get
+            {
+                return _accentColors;
+            }
+
+            set
+            {
+                if (_accentColors == value)
+                {
+                    return;
+                }
+
+                _accentColors = value;
+                RaisePropertyChanged(() => AccentColors);
+            }
+        }
+        #endregion
 
         ////public override void Cleanup()
         ////{
@@ -62,4 +141,42 @@ namespace CL.UI.WPF.ViewModel
         ////    base.Cleanup();
         ////}
     }
+
+    #region Themes bind classes
+    public class AccentColorMenuData
+    {
+        public string Name { get; set; }
+        public Brush BorderColorBrush { get; set; }
+        public Brush ColorBrush { get; set; }
+
+        private ICommand changeAccentCommand;
+
+        public ICommand ChangeAccentCommand
+        {
+            get { return this.changeAccentCommand ?? (changeAccentCommand = new RelayCommand<object>(
+                    x =>
+                    {
+                        DoChangeTheme(x);
+                    }));
+            }
+        }
+
+        protected virtual void DoChangeTheme(object sender)
+        {
+            var theme = ThemeManager.DetectAppStyle(Application.Current);
+            var accent = ThemeManager.GetAccent(this.Name);
+            ThemeManager.ChangeAppStyle(Application.Current, accent, theme.Item1);
+        }
+    }
+
+    public class AppThemeMenuData : AccentColorMenuData
+    {
+        protected override void DoChangeTheme(object sender)
+        {
+            var theme = ThemeManager.DetectAppStyle(Application.Current);
+            var appTheme = ThemeManager.GetAppTheme(this.Name);
+            ThemeManager.ChangeAppStyle(Application.Current, theme.Item2, appTheme);
+        }
+    }
+    #endregion
 }
